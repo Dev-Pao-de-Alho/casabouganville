7c8107549001b01fd8779c9e4b3b6efeb7b3aee7
Bruno Cabral <brunocabral035@gmail.com>
2023-01-19 18:24:56 -0300

    gallery done, need to fix image css

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 // Carrosel
+let aux = document.getElementById("images").style.transform;
+console.log(aux);
+
 document.getElementById("btnRight").addEventListener("click", () => {
-  document.getElementById("images").style.transform = "translateX(-100%)";
+  if (aux == "" || aux == "translateX(0%)") {
+    aux = "translateX(-100%)";
+  } else {
+    if (aux.length >= 18) {
+      aux = `translateX(${aux.slice(11, 16) - 100}%)`;
+    } else {
+      aux = `translateX(${aux.slice(11, 15) - 100}%)`;
+    }
+  }
+
+  if (aux === "translateX(-1200%)") {
+    aux = "translateX(0%)";
+  }
+  document.getElementById("images").style.transform = aux;
 });
 
 document.getElementById("btnLeft").addEventListener("click", () => {
-  document.getElementById("images").style.transform = "translateX(-0%)";
+  if (aux == "" || aux == "translateX(0%)") {
+    aux = "translateX(-1100%)";
+  } else {
+    if (aux.length >= 18) {
+      aux = `translateX(${Number(aux.slice(11, 16)) + 100}%)`;
+    } else {
+      aux = `translateX(${Number(aux.slice(11, 15)) + 100}%)`;
+    }
+  }
+  document.getElementById("images").style.transform = aux;
 });
 
-document.getElementById("first").addEventListener("click", () => {
-  document.getElementById("images").style.transform = "translateX(-0%)";
-});
+// -------------------------------------------------------------
 
-document.getElementById("second").addEventListener("click", () => {
-  document.getElementById("images").style.transform = "translateX(-100%)";
-});
+// Choice function
+function mudaCarrosel(id) {
+  let multiply = id * 100;
 
-document.getElementById("third").addEventListener("click", () => {
-  document.getElementById("images").style.transform = "translateX(-200%)";
-});
+  if (id == 0) {
+    document.getElementById(
+      "images"
+    ).style.transform = `translateX(-${multiply}%)`;
+    aux = `translateX(${multiply}%)`;
+  } else {
+    document.getElementById(
+      "images"
+    ).style.transform = `translateX(-${multiply}%)`;
+    aux = `translateX(-${multiply}%)`;
+  }
+}
 
+// -------------------------------------------------------------
